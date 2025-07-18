d82716bad165080e046f059fd757138b8d85a025
ProsteZvire <prostezvire@seznam.cz>
2025-07-18 21:46:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let isOpened = false;
+
 function openBox() {
-  document.querySelector('.box').classList.toggle('open');
+  if (isOpened) return;
+
+  document.getElementById('lid').style.transform = 'rotateX(-120deg)';
+  document.getElementById('light').style.opacity = '1';
+  document.getElementById('message').style.opacity = '1';
+
+  isOpened = true;
 }
